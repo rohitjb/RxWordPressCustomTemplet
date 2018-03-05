b6d5fa290a299e91c3bfb4bce4df3b5423ac504f
Rohit Sharma <rohit0690@gmail.com>
2018-03-05 23:25:07 +0530

    Removing all the commented code.

--- RxWordPressCustomTemplet/ViewController.swift
+++ RxWordPressCustomTemplet/ViewController.swift
@@ -1,23 +1,13 @@
-//
-//  ViewController.swift
-//  RxWordPressCustomTemplet
-//
-//  Created by Sharma, Rohit on 3/5/18.
-//  Copyright © 2018 Sharma, Rohit. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
